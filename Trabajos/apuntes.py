ea54cd65f291a6f408b0e5dff2ce503700bd1211
Camilo Londoño Alvarez <cristulxpeke@gmail.com>
2023-09-21 16:32:57 -0500

    semana 4 de clases

---
+++ Trabajos/apuntes.py
@@ -0,0 +1,4 @@
+num1 = int(input('ingrese por favor un numero '))
+num2 = int(input('ingrese por favor un numero '))
+division = num1//num2
+print ('el resultado de la division es ',division)
---
+++ Universidad/cuarto semestre/Analisis y diseño de algoritmos/Exposicion.py
@@ -0,0 +1,28 @@
+#orden constante
+def displayNum(n):
+    print(n)
+displayNum(42)
+print('---------')
+#Orden logarítmico = O(log n)
+def displayQuick(n):
+    k = 1
+    while k <= n:
+        print(k)
+        k *= 5
+displayQuick(10)
+print('---------')
+#Orden lineal = O(n)
+def sum1To(n):
+    sum = 1
+    for k in range(1, n + 1):
+        sum += k
+    return sum
+print (sum1To(10))
+print('---------')
+#Orden cuadrático = O(n2)
+def display(n):
+    for k in range(1, n + 1):
+        for j in range(1, n + 1):
+            print(j, end=' ')
+        print()
+display(5)
